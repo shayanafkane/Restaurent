faa47bfb150c742cff3940e40ce19946d3d98378
shayanafkane <shayanafkane@gmail.com>
2021-12-07 13:52:33 +0330

    Add-scroll-fixed-js

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+// scroll navbar
+let nav = document.querySelector('.navigation-wrap');
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("scroll-on")
+    }else{
+        nav.classList.remove("scroll-on")
+    }
+}
+
+
+// nav hide

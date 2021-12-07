c185acb84ee12082c4bcfaf140a378834136218c
shayanafkane <shayanafkane@gmail.com>
2021-12-07 13:59:25 +0330

    Add-js-mobile-menu-hide

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,21 @@
 // scroll navbar
 let nav = document.querySelector('.navigation-wrap');
-window.onscroll = function() {
-    if(document.documentElement.scrollTop > 20){
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
         nav.classList.add("scroll-on")
-    }else{
+    } else {
         nav.classList.remove("scroll-on")
     }
 }
 
 
 // nav hide
+
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+
+navBar.forEach(function (a) {
+    a.addEventListener("click", function () {
+        navCollapse.classList.remove("show")
+    })
+})
